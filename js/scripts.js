2a7b3a4e0abaa908b273403b19564f071538382c
grze2000 <programista333@gmail.com>
2021-07-21 00:33:21 +0200

    feat: add animations

--- js/scripts.js
+++ js/scripts.js
@@ -122,3 +122,130 @@ const observer = new MutationObserver(() => {
   if(terminal.scrollHeight > terminal.clientHeight) terminal.scrollTop = terminal.scrollHeight;
 });
 observer.observe(terminal, config);
+
+function revealElement(element, direction=-1) {
+  let x = 0, y = 0;
+  if(element.classList.contains('reveal-left')) {
+    x = -100;
+  } else if(element.classList.contains('reveal-right')) {
+    x = 100;
+  } else {
+    y = direction*100;
+  }
+  gsap.fromTo(element, {x: x, y: y, autoAlpha: 0}, {x: 0, y: 0, autoAlpha: 1, ease: 'expo', overwrite: 'auto', duration: 1.5});
+}
+
+document.querySelectorAll('.reveal').forEach(element => {
+  gsap.set(element, {autoAlpha: 0});
+  ScrollTrigger.create({
+    trigger: element,
+    onEnter: () => { revealElement(element, -1) },
+    onEnterBack: () => { revealElement(element, 1) },
+    onLeave: () => { gsap.set(element, {autoAlpha: 0}) }
+  })
+});
+
+let proxy = {skew: 0};
+let skewSetter = gsap.quickSetter('.skew', 'skewY', 'deg');
+let clamp = gsap.utils.clamp(-15, 15);
+
+ScrollTrigger.create({
+  onUpdate: (self) => {
+    let skew = clamp(self.getVelocity() / -500);
+    if(Math.abs(skew) > Math.abs(proxy.skew)) {
+      proxy.skew = skew;
+      gsap.to(proxy, {
+        skew: 0,
+        duration: 0.8,
+        ease: "power3",
+        overwrite: true,
+        onUpdate: () => skewSetter(proxy.skew)
+      })
+    }
+  }
+})
+
+gsap.set('.skew', {
+  transformOrigin: 'left center',
+  force3D: true
+})
+
+
+gsap.utils.toArray('.loc').forEach((element, i) => {
+  const index = i%4;
+  gsap.set(element, {
+    x: index%2 ? 100 : -100,
+    y: index<2 ? -100: 100,
+    scale: 0.5,
+    autoAlpha: 0,
+    rotation: [0, 3].includes(index) ? -15 : 15,
+    transformOrigin: `${index%2 ? 'right' : 'left'} ${index<2 ? 'bottom' : 'top'}`,
+    pin: true
+  });
+})
+
+gsap.utils.toArray('.images4').forEach((element) => {
+  Array.from(element.children).forEach((child) => {
+    gsap.to(child, {
+      x: 0,
+      y: 0,
+      scale: 1,
+      autoAlpha: 1,
+      rotation: 0,
+      scrollTrigger: {
+        trigger: element,
+        scrub: true,
+        start: 'top bottom',
+        end: 'top top'
+      }
+    })
+  });
+});
+
+gsap.to('.parallax-image__content', {
+  yPercent: -100,
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.parallax-image',
+    scrub: true
+  }, 
+});
+
+gsap.to('.parallax-image__image', {
+  yPercent: 50,
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.parallax-image',
+    scrub: true
+  }, 
+});
+
+const parallaxImage = document.querySelector('.parallax-image__image');
+
+function scalePrallax() {
+  const parallax = document.querySelector('.parallax-image');
+  const parallaxContent = document.querySelector('.parallax-image__content');
+  gsap.set(parallax, {
+    'padding-top': parallaxImage.height/2-parallaxContent.offsetHeight/2,
+    'min-height': parallaxImage.height*1.5
+  });
+}
+
+window.addEventListener('resize', scalePrallax);
+parallaxImage.addEventListener('load', scalePrallax);
+
+imagesLoaded('.carousel img', () => {
+  gsap.utils.toArray('.carousel__line').forEach(line => {
+    gsap.fromTo(line, {
+      x: line.classList.contains('carousel-left') ? -line.scrollWidth : '100%'
+    }, {
+      x: line.classList.contains('carousel-left') ? 0 : (line.scrollWidth-line.offsetWidth)*-1,
+      scrollTrigger: {
+        trigger: '.carousel',
+        scrub: 0.5,
+        start: 'top bottom',
+        end: 'top top'
+      }
+    });
+  });
+});
